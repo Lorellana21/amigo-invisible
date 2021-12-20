4a88cfa6b8142a4b7882d28695aec919fd0108b7
Lorellana <lorellana.ordonez@gmail.com>
2021-12-20 20:49:29 +0100

    add more players

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
-let playerList = [0, 1, 2, 3, 4];
+let playerList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let assignedPlayerToGift = [];
+const formElement = document.querySelector(".js-formSecond");
+const button = document.querySelector(".js-btn");
 
 function doMatches() {
   do {
@@ -11,7 +13,12 @@ function doMatches() {
     assignedPlayerToGift[1] === 1 ||
     assignedPlayerToGift[2] === 2 ||
     assignedPlayerToGift[3] === 3 ||
-    assignedPlayerToGift[4] === 4
+    assignedPlayerToGift[4] === 4 ||
+    assignedPlayerToGift[5] === 5 ||
+    assignedPlayerToGift[6] === 6 ||
+    assignedPlayerToGift[7] === 7 ||
+    assignedPlayerToGift[8] === 8 ||
+    assignedPlayerToGift[9] === 9
   );
   console.log(assignedPlayerToGift);
 }
@@ -22,12 +29,17 @@ function getPlayerName(i) {
   if (i === 2) return document.getElementById("player2").value;
   if (i === 3) return document.getElementById("player3").value;
   if (i === 4) return document.getElementById("player4").value;
+  if (i === 5) return document.getElementById("player5").value;
+  if (i === 6) return document.getElementById("player6").value;
+  if (i === 7) return document.getElementById("player7").value;
+  if (i === 8) return document.getElementById("player8").value;
+  if (i === 9) return document.getElementById("player9").value;
 }
 
 function showResults() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     alert(
-      getPlayerName(i) + "  regala a  " + getPlayerName(assignedPlayerToGift[i])
+      getPlayerName(i) + " regala a " + getPlayerName(assignedPlayerToGift[i])
     );
   }
 }
@@ -37,5 +49,4 @@ function handleSecretSanta() {
   showResults();
 }
 
-const button = document.querySelector(".js-btn");
 button.addEventListener("click", handleSecretSanta);
